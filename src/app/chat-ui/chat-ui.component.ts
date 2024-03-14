7c99e6eb5990f5b6cffb46e57f17011a61f76e01
Ghost <OverlyGenericAddress@pm.me>
2024-03-14 22:18:03 +0100

    Done some basic styling, still didn't fix height issues

--- src/app/chat-ui/chat-ui.component.ts
+++ src/app/chat-ui/chat-ui.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
 import { ChatService } from '../chat/chat.service';
 
 @Component({
@@ -6,7 +6,8 @@ import { ChatService } from '../chat/chat.service';
   templateUrl: './chat-ui.component.html',
   styleUrls: ['./chat-ui.component.scss']
 })
-export class ChatUiComponent {
+export class ChatUiComponent implements AfterViewChecked {
+  @ViewChild('messageContainer') private messageContainer!: ElementRef;
   
   // The inputField property is bound to the input field in the template.
   inputField: string = '';
@@ -14,7 +15,17 @@ export class ChatUiComponent {
   // Messages are managed by the ChatService and are passed to the ChatUiComponent via the observable.
   messages = this.chatService.messages;
   
-  constructor(private chatService: ChatService) {}
+  constructor(private chatService: ChatService) { }
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.messageContainer.nativeElement.scrollTop = this.messageContainer.nativeElement.scrollHeight;
+    } catch(err) { }
+  }
 
   // The inputUserMessage method is called when the user submits a new message.
   inputUserMessage() {
@@ -22,6 +33,8 @@ export class ChatUiComponent {
     if (this.inputField !== '') {
       // The ChatService is used to add a new usermessage to the history.
       this.chatService.userInputMessage(this.inputField);
+
+      this.scrollToBottom();
       
       // The input field is cleared.
       this.inputField = '';
