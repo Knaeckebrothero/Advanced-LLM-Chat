57035fa1384c9de9ffc996f8666e2380f1b3664a
Ghost <OverlyGenericAddress@pm.me>
2024-04-06 15:28:37 +0200

    Played around with swiping listeners

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, NgZone } from '@angular/core';
 import { StatusBarService } from './status-bar/status-bar.service';
 
 
@@ -8,11 +8,42 @@ import { StatusBarService } from './status-bar/status-bar.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Advanced LLM Chat';
+  @ViewChild('swipeArea') swipeArea!: ElementRef;
 
-  constructor(private statusService: StatusBarService) { }
+  title = 'Advanced LLM Chat';
+  private touchStartX = 0;
+  private touchEndX = 0;
+
+  constructor(private statusService: StatusBarService, private zone: NgZone) { }
+
+  ngAfterViewInit() {
+    this.addSwipeListeners();
+  }
 
   toggleNavbar() {
     this.statusService.toggleSidenav();
+  }
+
+  addSwipeListeners() {
+    this.zone.runOutsideAngular(() => {
+      const swipeElement = this.swipeArea.nativeElement;
+
+      swipeElement.addEventListener('touchstart', (event: TouchEvent) => {
+        this.touchStartX = event.touches[0].clientX;
+      }, { passive: true });
+
+      swipeElement.addEventListener('touchmove', (event: TouchEvent) => {
+        this.touchEndX = event.touches[0].clientX;
+      }, { passive: true });
+
+      swipeElement.addEventListener('touchend', () => {
+        if (this.touchStartX < this.touchEndX && Math.abs(this.touchStartX - this.touchEndX) > 50) {
+          // A right swipe gesture was detected
+          this.zone.run(() => {
+            this.toggleNavbar();
+          });
+        }
+      }, { passive: true });
+    });
   }
 }
