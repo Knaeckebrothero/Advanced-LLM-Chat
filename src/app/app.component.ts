49ac5ea1f9099ed1727c09277a746b0cd5f0a450
Ghost <OverlyGenericAddress@pm.me>
2024-04-06 15:55:21 +0200

    Removed the swiping option since it caused issues with klicking buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild, NgZone } from '@angular/core';
+import { Component } from '@angular/core';
 import { StatusBarService } from './status-bar/status-bar.service';
 
 
@@ -8,42 +8,11 @@ import { StatusBarService } from './status-bar/status-bar.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  @ViewChild('swipeArea') swipeArea!: ElementRef;
-
   title = 'Advanced LLM Chat';
-  private touchStartX = 0;
-  private touchEndX = 0;
 
-  constructor(private statusService: StatusBarService, private zone: NgZone) { }
-
-  ngAfterViewInit() {
-    this.addSwipeListeners();
-  }
+  constructor(private statusService: StatusBarService) { }
 
   toggleNavbar() {
     this.statusService.toggleSidenav();
-  }
-
-  addSwipeListeners() {
-    this.zone.runOutsideAngular(() => {
-      const swipeElement = this.swipeArea.nativeElement;
-
-      swipeElement.addEventListener('touchstart', (event: TouchEvent) => {
-        this.touchStartX = event.touches[0].clientX;
-      }, { passive: true });
-
-      swipeElement.addEventListener('touchmove', (event: TouchEvent) => {
-        this.touchEndX = event.touches[0].clientX;
-      }, { passive: true });
-
-      swipeElement.addEventListener('touchend', () => {
-        if (this.touchStartX < this.touchEndX && Math.abs(this.touchStartX - this.touchEndX) > 50) {
-          // A right swipe gesture was detected
-          this.zone.run(() => {
-            this.toggleNavbar();
-          });
-        }
-      }, { passive: true });
-    });
   }
 }
