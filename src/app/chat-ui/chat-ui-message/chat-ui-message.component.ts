2842474b1a4b1aefcedb3e67e73cdedcd314ecee
Knackebrothero <overlygenericaddress@pm.me>
2025-05-30 10:36:33 +0200

    Remove unnecessary blank line in ChatUiMessageComponent template

--- src/app/chat-ui/chat-ui-message/chat-ui-message.component.ts
+++ src/app/chat-ui/chat-ui-message/chat-ui-message.component.ts
@@ -13,7 +13,6 @@ import { ChatUiComponent } from '../chat-ui.component';
 export class ChatUiMessageComponent {
   // Pass the message object from the parent component
   @Input() message!: Message;
-
   // Variables
   editing: boolean = false;
   backupContent!: string;
